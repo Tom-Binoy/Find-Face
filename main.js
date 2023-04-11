fb0ca578b0c6859f49b65a2bad0acaf03feec774
Tom-Binoy <111070900+Tom-Binoy@users.noreply.github.com>
2023-04-11 20:30:48 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+s="";
+
+function sm(n){
+    console.log(s)
+    s = n;
+    row = "<img class='img-responsive' src='"+s+".png'>"
+        document.getElementById("snap").innerHTML = row ;
+        window.alert("The Image You Selected has Uploaded");
+       
+        $("#img").modal("hide");
+        showresult();
+}
+
+
+console.log("ml5 Version",ml5.version);
+classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/zElHSu2ty/model.json",modelLoaded)
+function modelLoaded(){
+    console.log("modelLoaded");
+}
+function showresult(){
+    img= s+'.png'
+    classifier.classify(img,gotResult);
+}
+
+function gotResult(error,result){
+    if(error){
+        console.error(error);
+    }
+    else{
+        console.log(result);
+        window.alert("Then scroll down to view the result");
+
+        document.getElementById('name').innerHTML = result[0].label;
+        document.getElementById('accuracy').innerHTML = (result[0].confidence*100).toFixed(2)+'%';
+    }
+}
+
